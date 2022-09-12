5aec24b9ca6e16abdb1dbb1bd9f61755d3215a57
Emediong Benison <emediongbenison@yahoo.com>
2022-09-13 00:37:17 +0100

    using create-react-app to create a react project

---
+++ week1/02-using_cra/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World</h1>
+      <p>This template is created using the npx create-react-app app-name (CRA)</p>
+      <p>However, the unnecessary files were deleted.</p>
+    </div>
+  );
+}
+
+export default App;
